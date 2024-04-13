2e4c45b2e3d300a9695517500ed6c466495b9d4c
jordan <pastoraljordan23@gmail.com>
2024-04-13 16:37:47 -0700

    adding llamaindex

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # rag_hackathon
 
 Hackathon project for CMPE 172, Spring 2024.
+### Run these commands in your terminal first (May be different depending on your system)
+- pip install llama-index
+- pip install llama-index-llms-ollama
+- pip install llama-index-embeddings-huggingface
+
+### After installing, run `ollama pull gemma:2b` in the terminal
+
+### Code should be good to run
---
+++ test.py
@@ -0,0 +1,24 @@
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.core.embeddings import resolve_embed_model
+from llama_index.llms.ollama import Ollama
+
+# This is without context
+gemma2b = Ollama(model="gemma:2b", request_timeout=30.0)
+resp = gemma2b.complete("what are all michael jordan's accolades")
+print(resp)
+
+
+# This is with context
+documents = SimpleDirectoryReader("./data").load_data()
+
+Settings.embed_model = resolve_embed_model("local:BAAI/bge-small-en-v1.5")
+
+Settings.llm = Ollama(model="gemma:2b", request_timeout=30.0)
+
+index = VectorStoreIndex.from_documents(
+    documents,
+)
+
+query_engine = index.as_query_engine()
+response = query_engine.query("list major food insecurity reason")
+print(response)
