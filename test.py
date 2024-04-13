6dd5db8f302622f317787870ea966ad96423ac15
sZeru <kolby.salmond@yahoo.com>
2024-04-13 12:21:39 -0700

    Added lines to run test.py.

--- test.py
+++ test.py
@@ -4,9 +4,11 @@ Created on Sat Apr 13 11:52:38 2024
 
 @author: cdare
 """
-
 from flask import Flask
+
 app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+app.run(host="0.0.0.0", port=80)
